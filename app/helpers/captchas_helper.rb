0ff87e1f706459f2939cca13e395278b33fd972f
Rahul Soni <rahulsoni@Rahuls-MacBook-Air.local>
2025-01-23 22:52:23 +0530

    Update captcha refersh timing

--- app/helpers/captchas_helper.rb
+++ app/helpers/captchas_helper.rb
@@ -1,11 +1,19 @@
 module CaptchasHelper
-  def random_text_generator
+  def generate_unique_for_user(user)
+    loop do
+      captcha = random_text
+      unless user.captchas.where(captcha_text: captcha).exists?
+        return captcha
+      end
+    end
+  end
+
+  def random_text
     numbers = [*'0'..'9'].sample(2)
     uppercase = [*'A'..'Z'].sample(2)
     lowercase = [*'a'..'z'].sample(2)
-    remaining = ([*'0'..'9', *'A'..'Z', *'a'..'z'] - (numbers + uppercase + lowercase)).sample(2)
+    remaining = ([*'0'..'9', *'A'..'Z', *'a'..'z'] - (numbers + uppercase + lowercase)).sample(4)
 
     (numbers + uppercase + lowercase + remaining).shuffle.join
   end
-
 end
--- app/javascript/controllers/captcha_controller.js
+++ app/javascript/controllers/captcha_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    let time = 15;
+    let time = 13;
     const timerElement = document.getElementById('time');
     const form = document.getElementById('captcha-form');
 
